714f445833f9fb2c2d15c930618979d6548b1298
Henri Vänskä <henri.vanska@student.laurea.fi>
2025-12-17 14:12:16 +0200

    Fix indentation for password length check
    Fix indentation for password length checked

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def generate_password(length):
 # Args: length (int): The desired length of the password.
 # Returns: str: A random strong password.
   if length < 8: 
-        length = 8 # antaa 8 merkkiä vaikka valittaisiin alle 8
+    length = 8 # antaa 8 merkkiä vaikka valittaisiin alle 8
     characters = string.ascii_letters + string.digits + string.punctuation # kaikki aakkoset, numerot ja erikoismerkit
     return ''.join(random.choice(characters) for _ in range(length)) # arvotaan satunnaiset merkit, .join yhdistää ne merkkijonoksi
 
